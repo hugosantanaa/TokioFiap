a35196317b5460c6064688d08736ee0202f5b9e8
hugosantanaa <hugo.s2005@hotmail.com>
2023-04-26 14:31:37 -0300

    atualizado com exe

--- workspace/FiapTokio/src/logica/exercicios/aula06/Exercicio10.java
+++ workspace/FiapTokio/src/logica/exercicios/aula06/Exercicio10.java
@@ -6,11 +6,33 @@ public class Exercicio10 {
 
 	public static void main(String[] args) {
 		
-		double calc = 0,salario = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor do seu salário"));
-		
+		double salario = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor do seu salário"));
+		double calc = 0;
 		if (salario <=280 ) {
-			calc = (salario*100)/salario;
-			 JOptionPane.showMessageDialog(null, "O valor do seu salario "+salario+", sofreu um aumento de 20% e ficou "+calc);
+			calc = (salario*20)/100;
+			JOptionPane.showMessageDialog(null, "O seu salario "+salario+"\n"
+					+ "Recebeu um aumento de 20%\n"
+					+"No valor de "+calc+"\n"
+					+"E ficou "+(calc+salario));
+		}else if(salario>280 && salario <= 700) {
+			calc = (salario*15)/100;
+			JOptionPane.showMessageDialog(null,  "O seu salario "+salario+"\n"
+					+ "Recebeu um aumento de 15%\n"
+					+"No valor de "+calc+"\n"
+					+"E ficou "+(calc+salario));
+		}else if(salario >700 && salario <=1500) {
+			calc = (salario*10)/100;
+			JOptionPane.showMessageDialog(null, "O seu salario "+salario+"\n"
+					+ "Recebeu um aumento de 10%\n"
+					+"No valor de "+calc+"\n"
+					+"E ficou "+(calc+salario));
+		}else {
+			calc = (salario*5)/100;
+			JOptionPane.showMessageDialog(null,  "O seu salario "+salario+"\n"
+					+ "Recebeu um aumento de 5%\n"
+					+"No valor de "+calc+"\n"
+					+"E ficou "+(calc+salario));
+
 		}
 	}
 
